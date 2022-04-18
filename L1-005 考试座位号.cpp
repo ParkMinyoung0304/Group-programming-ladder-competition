cd1f17994b93dafa93347a495fda9736968e94d3
ParkMinyoung0304 <62382458+ParkMinyoung0304@users.noreply.github.com>
2022-04-18 10:40:02 +0800

    Update L1-005 考试座位号.cpp

--- L1-005 考试座位号.cpp
+++ L1-005 考试座位号.cpp
@@ -2,27 +2,25 @@
 #include<cstring>
 using namespace std;
 int count;
-struct point{
+struct point{           //定义一个类型，通过对象来存储
     string id;
     int kaoshi_num;
 };
 int main(){
     cin>>count;
     point stu;
-    point num[1005];
+    point num[1005];        //初始化一个对象数组
     int shiji_num;
     for(int i = 0;i<count;i++){
         cin>>stu.id>>shiji_num>>stu.kaoshi_num;
         num[shiji_num] = stu;
     }
     int search_count;
-    int tem[1005];
+    int tem;         
     cin>>search_count;
     for(int j=0;j<search_count;j++){
-        cin>>tem[j];
-    }
-    for(int k=0;k<search_count;k++){
-        cout<<num[tem[k]].id<<" "<<num[tem[k]].kaoshi_num<<endl;
+        cin>>tem[j];        
+        cout<<num[tem].id<<" "<<num[tem].kaoshi_num<<endl;
     }
     return 0;
 }
